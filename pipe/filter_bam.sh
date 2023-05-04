0ea9713d2f91e3fb47256189b38a8c5aa9027cad
mjheid <mjheid@github.com>
2023-05-04 13:35:19 +0200

    delete old starting point files and dirs

--- pipe/filter_bam.sh
+++
@@ -1,6 +0,0 @@
-# filter unmapped reads
-samtools view -b -f 4 ./hf1.d15.bam > ./hf1.d15.filtered.bam
-# filter transgene reads
-samtools view -b  ./hf1.d15.bam GFP >> ./hf1.d15.filtered.bam
-# dont execute if CellTag UTR not included in ref
-samtools view -b ./hf1.d15.bam CellTag.UTR >> ./hf1.d15.filtered.bam
